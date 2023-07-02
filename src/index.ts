b65ced2ef574d0dd47f793d1c9a4d77c4c5917b5
李康 <elsa_likang@126.com>
2023-07-02 19:52:09 +0800

    feat:新增数据联动组件

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { Export } from './Export';
 export { Category } from './Category';
 export { default as Editor } from './Editor';
 export { DragSort } from './Table';
+export { DependencyForm } from './Form';
 export { default as Upload } from './Upload';
 export { Crypto } from './utils';
